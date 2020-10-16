876c548c47d8041e19439e00842922235d4a965c
Manny <egalli64@gmail.com>
2020-10-16 11:52:25 +0200

    more examples

--- src/app/simple.service.spec.ts
+++ src/app/simple.service.spec.ts
@@ -1,32 +1,50 @@
 import { TestBed } from '@angular/core/testing';
 
-import { MathService } from './simple.service';
+import { SimpleService } from './simple.service';
 
-describe('MathService', () => {
-  let service: MathService;
+describe('SimpleService', () => {
+  let service: SimpleService;
 
   beforeEach(() => {
     TestBed.configureTestingModule({});
-    service = TestBed.inject(MathService);
+    service = TestBed.inject(SimpleService);
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
   });
 
+  // test number
   it('should negate', () => {
     const result = service.negate(42);
 
     expect(result).toBe(-42);
   });
 
-  it('should fail, by intentional mistake', () => {
-    const result = service.negate(42);
+  // disable test + show failures
+  it('should fail by intentional mistake', () => {
+    // comment the pending call to let the test fail
+    pending();
 
+    const result = service.negate(42);
     expect(result).toBe(42);
   });
+  
+  // test string to contain substring
+  it('should greet Tom', () => {
+    const name = 'Tom';
+    const result = service.hello(name);
 
-  it('should be skipped', () => {
-    pending();
+    expect(result).toContain(name);
+  });
+
+  // test iterable
+  it('should see all team members', () => {
+    const names = ['Bob', 'Kim', 'Jim'];
+    const result = service.team();
+
+    names.forEach(name => {
+      expect(result).toContain(name);
+    });
   });
 });
--- src/app/simple.service.ts
+++ src/app/simple.service.ts
@@ -3,11 +3,19 @@ import { Injectable } from '@angular/core';
 @Injectable({
   providedIn: 'root'
 })
-export class MathService {
+export class SimpleService {
 
   constructor() { }
 
   negate(value: number) {
     return -value;
+  }
+
+  hello(name: string) {
+    return 'Hello, ' + name;
+  }
+
+  team() {
+    return ['Bob', 'Kim', 'Jim'];
   }
 }
