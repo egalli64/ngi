b5171f4cad3fa43db61f4c3e497564fe24099b13
Manny <egalli64@gmail.com>
2020-10-26 15:29:18 +0100

    major cleanup

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -7,18 +7,6 @@ import { HttpClientModule } from '@angular/common/http';
 
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello/hello.component';
-import { User07Component } from './user07/user07.component';
-import { User09Component } from './user09/user09.component';
-import { User15Component } from './user15/user15.component';
-import { User17Component } from './user17/user17.component';
-import { Users08Component } from './users08/users08.component';
-import { Users09Component } from './users09/users09.component';
-import { Users15Component } from './users15/users15.component';
-import { Users17Component } from './users17/users17.component';
-import { Users27Component } from './users27/users27.component';
-import { AddItemComponent } from './add-item/add-item.component';
-import { UserFormComponent } from './user-form/user-form.component';
-import { UserRFormComponent } from './user-rform/user-rform.component';
 import { Routing1Component } from './routing1/routing1.component';
 import { Routing2Component } from './routing2/routing2.component';
 import { Routing3Component } from './routing3/routing3.component';
@@ -59,18 +47,6 @@ const appRoutes: Routes = [
   declarations: [
     AppComponent,
     HelloComponent,
-    User07Component,
-    User15Component,
-    User17Component,
-    User09Component,
-    Users08Component,
-    Users09Component,
-    Users15Component,
-    Users17Component,
-    Users27Component,
-    AddItemComponent,
-    UserFormComponent,
-    UserRFormComponent,
     Routing1Component,
     Routing2Component,
     Routing3Component,
